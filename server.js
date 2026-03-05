c3010dc84b0cab614095d0fe26ca5902e0f64bec
Meghana <meghanamekala@gmail.com>
2026-03-05 15:23:28 +0530

    Fix email transport

--- server.js
+++ server.js
@@ -463,15 +463,10 @@ appendCustomerLog(customer, quoteNo, productType);
     });
 
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
    
